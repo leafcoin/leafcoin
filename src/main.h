232928086d813d4c4442b6a4947ae4b923bb8059
root <root@ubuntu.(none)>
2014-01-25 18:26:49 +0100

    leafcoin

--- src/main.h
+++ src/main.h
@@ -54,7 +54,7 @@ static const int64 DUST_SOFT_LIMIT = 100000000;
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 1000000;
 /** No amount larger than this (in satoshi) is valid */
-static const int64 MAX_MONEY = 2100000000 * COIN; // DogeCoin: maximum of 100B coins (given some randomness), max transaction 10,000,000,000 for now
+static const int64 MAX_MONEY = 2100000000 * COIN; // LeafCoin: maximum of 100B coins (given some randomness), max transaction 10,000,000,000 for now
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 26;
@@ -623,7 +623,7 @@ public:
     {
         // Large (in bytes) low-priority (new, small-coin) transactions
         // need a fee.
-        return dPriority > 100 * COIN * 1440 / 250; // DogeCoin: 1440 blocks found a day. Priority cutoff is 100 leafcoin day / 250 bytes.
+        return dPriority > 100 * COIN * 1440 / 250; // LeafCoin: 1440 blocks found a day. Priority cutoff is 100 leafcoin day / 250 bytes.
     }
 
 // Apply the effects of this transaction on the UTXO set represented by view
