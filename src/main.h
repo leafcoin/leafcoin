38f60c95397187d8e853157c2867034c074b52c4
root <root@ubuntu.(none)>
2014-01-26 03:10:15 +0100

    leafcoin reward fix

--- src/main.h
+++ src/main.h
@@ -54,7 +54,7 @@ static const int64 DUST_SOFT_LIMIT = 100000000;
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 1000000;
 /** No amount larger than this (in satoshi) is valid */
-static const int64 MAX_MONEY = 2100000000 * COIN; // LeafCoin: maximum of 100B coins (given some randomness), max transaction 10,000,000,000 for now
+static const int64 MAX_MONEY = 21000000000 * COIN; // LeafCoin: maximum of 100B coins (given some randomness), max transaction 10,000,000,000 for now
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 26;
